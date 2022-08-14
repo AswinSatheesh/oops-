187eddd076ac98de0f2e0c5c1654ac0d1e972bf9
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-08-14 15:59:20 +0530

    getter and setter learned

---
+++ aug14/GetterSetter/GetterSetter.java
@@ -0,0 +1,13 @@
+class GetterSetter{
+    private String name;
+
+    public void setName(String name) //setter 
+    {
+        this.name = name;
+    }
+
+    public String getName() // getter must have return value
+    {
+        return name;
+    }
+}
---
+++ aug14/GetterSetter/Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String args[])
+    {
+        GetterSetter obj = new GetterSetter();
+        obj.setName("Aswin");
+        System.out.println(obj.getName());
+    }
+}
